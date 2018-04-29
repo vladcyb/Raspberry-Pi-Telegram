fb18f040f4d0d154e6272835705e95351dce96df
vladcyb <vladcyb@users.noreply.github.com>
2018-04-29 03:18:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 import RPi.GPIO as GPIO
 from datetime import datetime
 
-URL = 'https://api.telegram.org/botYOUR_TOKEN/' # Replace YOUR_TOKEN on the token of your bot
+URL = 'https://api.telegram.org/botYOUR_TOKEN/' # Replace YOUR_TOKEN with the token of your bot
 
 YOUR_TELEGRAM_ID = 159619537 # Change on your Telegram id
 
